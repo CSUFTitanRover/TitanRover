f121e9dcc665d551c47c9b70324e1a44cf846fa2
Joseph Porter <joporter5606@live.com>
2016-10-10 13:43:47 -0700

    Added a database to homebase_server.js

--- README.md
+++ README.md
@@ -15,12 +15,27 @@ Launch the app
 
 ## Run servers
 
-In one terminal run the homebase server with
+### Need to set up the MongoDB to get it working
 
-```node homebase_server.js```
+1. First download MongoDB for your system
+   [Mongo Website](https://www.mongodb.com/)
+2. Create /data/db directories in your root folder
+3. Then navigate to the bin directory of the downloaded folder
+4. Run the mongo server (needs to have root permissions)
+	```sudo mongod --port 6969```
+5. Then in other terminal run
+	```sudo mongo --port 6969```
+	This should connect you with the mongodb server running in other terminal
+6. Run the homebase server
+	```node homebase_server.js```
+7. Run the rover server to post data to homebase
+	```node rover_server.js```
 
-then in other terminal run the rover server and it should post data to homebase server
 
-```node rover_server.js```
+To see if data was stored in the database in the terminal with mongo client running run this command
+	```db.data.find()```
+This should show all the data inserted into the database
+
 
 If you want to change the data passed in the rover_server.js file modify the post_data object to whatever you want
+*Will need to have id in order for it to work*
--- homebase_server.js
+++ homebase_server.js
@@ -2,11 +2,35 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser')
 var app = express()
+var mongo = require('mongodb').MongoClient;
+
+// Will be set when we connect to the MongoDB
+var database;
+
+
+// Start the server
+var server = app.listen(3000, function() {
+	console.log("============ Server is up and running on port: ", server.address().port, "=============");
+});
+
+
+// Connect to the mongodb I changed it to port 6969 cause im dirty and the
+// default port of 27017 was not working for me for some reason
+mongo.connect("mongodb://localhost:6969/test", function(err, db) {
+	if (!err) {
+		console.log("=========== Connected to MongoDB =============");
+		database = db
+	}
+	else {
+		console.log("########## Failed to Connect to Database ##########");
+		throw new Error("Terminating the server database not started");
+	}
+
+});
 
 // parse application/json
 app.use(bodyParser.json())
 
-console.log("Creating the server");
 
 app.use('/data', function(req, res, next) {
     console.log("======= Storing Data ========");
@@ -23,18 +47,31 @@ app.post('/data', function(req, res, next) {
     switch(request.id)
     {
         case 0:
-            fs.appendFile('mobility.json', JSON.stringify(request) + '\n');
+			// Store in the database
+            database.collection('data').insertOne(request);
             console.log("Storing mobility data");
             break;
         case 1:
-            fs.appendFile('sensor.json', JSON.stringify(request) + '\n');
+			// Store in the database
+            database.collection('data').insertOne(request);
             console.log("Storing sensor data");
             break;
         // Make more for the sensors
+
+		default:
+			// The ID does not match or is not included
+			res.sendStatus(400);
     }
 
 
     res.sendStatus(200);
 });
 
-app.listen(3000);
+// Shutdown the server and close the database on ctrl+c press
+// This shutdowns the connections gracfully
+process.on( 'SIGINT', function() {
+	console.log("\n####### Server shutting down #######\n");
+	database.close();
+  // some other closing procedures go here
+	process.exit( );
+});
