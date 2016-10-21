93ab54fae5a1b4def1da704f065df3f8aa3ae71c
Rogelio Negrete <rogelio_negrete@live.com>
2016-10-21 00:17:19 -0700

    Added routing for the links

---
+++ roverUI/src/components/Modules_Dir/FourOhFour.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FourOhFour extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Uh-oh. looks like you found the 404 error page!.
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default FourOhFour;
---
+++ roverUI/src/components/Modules_Dir/Module2.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Module2 extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    I wonder... I wonder, if there in your mind,
+                    Do you still remember that moment behind?
+                    That singular second; that instant of then;
+                    When everything stopped, and the world changed again?
+                    I wonder... I wonder, if there in your dreams,
+                    Is everything different or still as it seems?
+                    Do you ever pause to consider the Earth?
+                    Do you ever ask what security's worth?
+                    I wonder... I wonder, if there in your world,
+                    Has empathy, wisdom, or hatred uncurled?
+                    Are you more distrustful, or do you recall:
+                    United we stand, but divided we fall?
+                    Remember, whatever your future may be,
+                    I hope that that it's happy and healthy and free.
+                    Remember to love, and to laugh, and to give.
+                    Remember September 11th, and live.
+
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Module2;
