fd98ac8a1dc8bb93a85084f59949fff3632035d1
Kevin Ochoa <ochoakevinm@gmail.com>
2016-10-27 12:25:37 -0700

    Update Modules_List.js

--- roverUI/src/components/Modules_List.js
+++ roverUI/src/components/Modules_List.js
@@ -4,6 +4,7 @@ import Chart1 from './modules_dir/Chart1';
 import Module2 from './modules_dir/Module2';
 import Chart2 from './modules_dir/Chart2';
 import Chart3 from './modules_dir/Chart3';
+import CameraModule from './modules_dir/CameraModule';
 
 // grouping modules into a nice package ;)
-export default {FourOhFour, Overview, Chart1, Module2, Chart2, Chart3}
+export default {FourOhFour, Overview, Chart1, Module2, Chart2, Chart3, CameraModule}
