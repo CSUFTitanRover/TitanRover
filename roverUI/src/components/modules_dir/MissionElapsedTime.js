bf165aed420153d7ae0f4ae8fdf80642246c4f94
weffe <rogelio_negrete@live.com>
2016-10-31 20:23:00 -0700

    Fixed a bug with auto-starting the stopwatch for the MET

--- roverUI/src/components/modules_dir/MissionElapsedTime.js
+++ roverUI/src/components/modules_dir/MissionElapsedTime.js
@@ -5,8 +5,11 @@ class MissionElapsedTime extends Component {
     constructor(props) {
         super(props);
 
+        let savedElapsedTime = parseInt(localStorage.getItem('savedElapsedTime'), 10);
+        let savedIsRunning = (localStorage.getItem('savedIsRunning') == "true");  // convert savedIsRunning (str) to a bool type
+
         // load in savedElapsedTime for persistence or default to 0
-        this.elapsedTime = parseInt(localStorage.getItem('savedElapsedTime'), 10) || 0;
+        this.elapsedTime = savedElapsedTime || 0;
         let d = new Date(); // temp date obj
         this.date = d.getFullYear()+'/'+d.getDate()+'/'+(d.getMonth()+1); // date for current day
         this.interval = null;
@@ -15,15 +18,16 @@ class MissionElapsedTime extends Component {
         this.handleReset = this.handleReset.bind(this);
 
         this.state = {
-            isRunning: false,
+            // load in savedIsRunning or default to false
+            isRunning: ( savedIsRunning || false),
             resetDisabled: false,
             // load in savedElapsedTime for persistence or default to 0
-            elapsedTotalTime: (parseInt(localStorage.getItem('savedElapsedTime'), 10) || 0)
+            elapsedTotalTime: ( savedElapsedTime || 0)
         };
     }
 
     tick() {
-        this.elapsedTime = this.elapsedTime + 1;
+        this.elapsedTime += 1;
         this.setState({elapsedTotalTime: this.elapsedTime});
         localStorage.setItem('savedElapsedTime', this.elapsedTime);
     }
@@ -36,43 +40,45 @@ class MissionElapsedTime extends Component {
         else {
             // starting stopwatch
             this.setState({isRunning: true, resetDisabled: true});
+            localStorage.setItem('savedIsRunning', 'true');
 
             //set the interval for every second
             this.interval = setInterval(this.tick, 1000);
-
         }
     }
 
     handleStop() {
-        console.log('Stopped elapsedTime: ' + this.elapsedTime);
         this.setState({isRunning: false, resetDisabled: false});
+        localStorage.setItem('savedIsRunning', 'false');
         clearInterval(this.interval);
     }
 
     handleReset() {
         this.elapsedTime = 0;
         this.setState({elapsedTotalTime: 0});
-        localStorage.removeItem('savedElapsedTime')
+        localStorage.removeItem('savedElapsedTime');
     }
 
     formatTime(passed_time) {
-        var sec_num = passed_time;
-        var hours   = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        let sec_num = passed_time;
+        let hours   = Math.floor(sec_num / 3600);
+        let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        let seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-        if (hours   < 10) {hours   = "0"+hours;}
-        if (minutes < 10) {minutes = "0"+minutes;}
-        if (seconds < 10) {seconds = "0"+seconds;}
+        if (hours   < 10) {hours   = "0" + hours;}
+        if (minutes < 10) {minutes = "0" + minutes;}
+        if (seconds < 10) {seconds = "0" + seconds;}
 
         return hours+':'+minutes+':'+seconds;
     }
 
     componentDidMount() {
-        // check if elapsedTime loaded in a value from localStorage
-        if (this.elapsedTime > 0) {
+        // check if isRunning is true via loaded from localStorage.savedIsRunning
+        if (this.state.isRunning == true) {
             // if so we auto-start the timer
-            this.handleStartAndStop();
+            this.setState({resetDisabled: true});
+            //set the interval for every second
+            this.interval = setInterval(this.tick, 1000);
         }
     }
 
