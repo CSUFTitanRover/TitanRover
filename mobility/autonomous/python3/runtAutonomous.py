0f7a7dca61ead4c4cf7999cb90d70f81cf569631
HawkinsonB <brandon.hawkinson@icloud.com>
2017-02-09 22:14:48 -0800

    Made comment changes with Tim for him to implement the IMU and Adafruit GPS code. Also added some TODOs on the file to layout basic logic for further implementation

--- mobility/autonomous/python3/runtAutonomous.py
+++ mobility/autonomous/python3/runtAutonomous.py
@@ -7,6 +7,12 @@ degToRad = math.pi / 180 #0.017453
 radToDeg = 180 / math.pi #57.29577795
 #----------------- end
 
+#TODO Add distance formula to calculate distance between two waypoints
+#TODO Add IMU and Adafruit GPS implimentation for testing
+#-THEN Figuring out the turning algorithm
+#--THEN Begin navigation towards waypoint function
+#---THEN Incorporate object recognition if needed 
+
 class waypoint:
     #TODO Add heading argument
     def __init__(self, targetLongitude, targetLattitude):
@@ -34,6 +40,8 @@ class waypoint:
                 math.cos(targetLattitudeRad) * math.cos(targetLongitudeRad - currentLongitudeRad))
         targetHeading = (angle * radToDeg + 360) % 360
         #end Timothy Parks' math ---------
+        #TODO Compare to current heading, calculate the difference of the two. The function will spit out the change in heading we need to make
+        
 
 #this function prompts for the longitude and lattitude of a desired waypoint.
 #these will be stored from first entered to last, enter your path in order
@@ -41,7 +49,6 @@ def enterWaypoint(waypoints_list = []):
     print("Please enter waypoint")
     inputLongitude = input("Longitude: ")
     inputLattitude = input("Lattitude: ")
-    #TODO: Get heading and add argument to input
     storeWaypoint = waypoint(inputLongitude, inputLattitude)
     waypoints_list.append(storeWaypoint) 
     return waypoints_list #return the appended list
@@ -58,6 +65,9 @@ def main():
             waypointsRecieved = True
     
     for x in range(len(waypoints_list)):
+        #Using this same logic we can call calculateHeading for each waypoint, then start the navigation to turn to the heading
+        #Once we've turned, start the navigation towards the wayppoint. Check if there. Then loop until all waypoints are finished
+        #Once finished spit back to the user that the loop has been completed OR call tennisBall recognition function/file/etc
         print (waypoints_list[x].longitude)
         print (waypoints_list[x].lattitude)
 
