1f9031f844770dfd80064691951a9f34cc7feafa
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2017-02-10 18:45:50 -0800

    Added Ultimate GPS to runtRover code

--- mobility/autonomous/python3/runtAutonomous.py
+++ mobility/autonomous/python3/runtAutonomous.py
@@ -1,11 +1,34 @@
 import os
 import math
+import serial, subprocess, os, time, gps, string, sys, pynmea2
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPSFIX = 18
+GPIO.setup(GPSFIX, GPIO.IN)
 
 #implementing Timothy Parks' math to calculate heading
 pi = math.pi #3.1415926535897932384626433832795
 degToRad = math.pi / 180 #0.017453
 radToDeg = 180 / math.pi #57.29577795
 #----------------- end
+
+#################################################################
+#on RPi installed minicom
+#sudo apt-get install minicom
+#
+#linked the minicom to port for use by serial
+#sudo minicom -b 9600 -o -D /ttyAMA0
+#pynmea2 must be downloaded and install
+#TX->RX
+#RX->TX
+
+ser=serial.Serial('/dev/ttyAMA0', 9600)
+#ser.stopbits = serial.STOPBITS_TWO
+#dev=subprocess.check_output('ls /dev/ttyAMA0', shell = True)
+#ser=serial.Serial(dev.strip(),baudrate=9600) #, stopbits=serial.STOPBITS_TWO)
+#################################################################
+
 
 #TODO Add distance formula to calculate distance between two waypoints
 #TODO Add IMU and Adafruit GPS implimentation for testing
@@ -15,33 +38,57 @@ radToDeg = 180 / math.pi #57.29577795
 
 class waypoint:
     #TODO Add heading argument
+    ###########################################################################
+    #all stored Longitude, Latitude and Headings need to be in DECIMAL DEGREES
+    ###########################################################################
     def __init__(self, targetLongitude, targetLattitude):
        self.longitude = targetLongitude #stores the target longitude to the waypoint
        self.lattitude = targetLattitude #stores the target lattitude to the waypoint
     
     #function takes in all current location data and compares it to the desired location data to determine target heading
     #Most math provided by Timothy Parks
-    def calculateHeading(currentLongitude, currentLattitude, currentHeading):
+    def calculateHeading(targetLongitude, targetLattitude, currentHeading): #currentLongitude, currentLattitude, currentHeading):
         #convert longitude and lattitude to radians
         targetLongitudeRad = targetLongitude * degToRad
         targetLattitudeRad = targetLattitude * degToRad
-        currentLongitudeRad = currentLongitude * degToRad
-        currentLattitudeRad = currentLattitude * degToRad
-        #--------
+        #currentLongitudeRad = currentLongitude * degToRad
+        #currentLattitudeRad = currentLattitude * degToRad
+		currentLongitudeRad, currentLattitudeRad = self.newPointFromGPS()         
+		#--------
 
         if (math.cos(targetLattitudeRad)*math.sin(targetLongitudeRad - currentLongitudeRad)) == 0:
             if targetLattitudeRad > currentLattitudeRad:
-                return 0.0
+                targetHeading = 0.0 #return 0.0
             else:
-                return 180
+                targetHeading = 180 #return 180
         else:
             angle = math.atan2(math.cos(targetLattitudeRad) * math.sin(targetLongitudeRad - currentLongitudeRad),
                 math.sin(targetLattitudeRad) * math.cos(currentLattitudeRad) - math.sin(currentLattitudeRad) *
                 math.cos(targetLattitudeRad) * math.cos(targetLongitudeRad - currentLongitudeRad))
         targetHeading = (angle * radToDeg + 360) % 360
+	
+		#Returns the degree change needed with currentHeading as a 0 degree using +- 180 degree change 
+		if (360 - currentHeading) + targetHeading < 180:
+	    	return (360 - currentHeading) + targetHeading 
+		else:
+	    	return targetHeading - currentHeading
+
         #end Timothy Parks' math ---------
         #TODO Compare to current heading, calculate the difference of the two. The function will spit out the change in heading we need to make
-        
+    
+    #New or updating the stored waypoint in the object
+    def newPointFromGPS()
+		if GPIO.input(GPSFIX)
+			nmeaSentance = ser.readline()
+			while not nmeaSentance[0:6] == '$GPGGA':
+	    		nmeaSentance = ser.readline()
+			nmea_Obj = pynmea2.parse(nmea2)
+			return nmea_obj1.latitude, nmea_obj1.longitude
+		else:
+			return 33.2222, 110.33434
+
+
+
 
 #this function prompts for the longitude and lattitude of a desired waypoint.
 #these will be stored from first entered to last, enter your path in order
