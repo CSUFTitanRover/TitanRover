01ab3d571ad8eb4712705fda0117e59e731ace7d
weffe <rogelio_negrete@live.com>
2017-02-21 16:19:43 -0800

    updated roverUI package.json

--- roverUI/src/routes/mission/modules/ArmControl.js
+++ roverUI/src/routes/mission/modules/ArmControl.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import BaseModuleTemplate from '../../../templates/BaseModuleTemplate';
+import Konva from 'konva';
+import {Layer, Rect, Stage} from 'react-konva';
 
 class ArmControl extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'green'
+        }
+    }
+
+    handleClick = () => {
+        let newColor = Konva.Util.getRandomColor();
+        this.setState({color: newColor});
+    };
+
     render() {
         return (
             <BaseModuleTemplate moduleName="Arm Control">
@@ -10,6 +24,17 @@ class ArmControl extends Component {
                     Kinematics if we get it working.
                 </p>
                 <img src="http://www.alistairwick.com/assets/images/robot/ik.gif"/>
+
+                <Stage width={700} height={700}>
+                    <Layer>
+                        <Rect
+                            x={10} y={10} width={50} height={50}
+                            fill={this.state.color}
+                            onClick={this.handleClick}
+                            draggable={true}
+                        />
+                    </Layer>
+                </Stage>
             </BaseModuleTemplate>
         );
     }
