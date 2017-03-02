6ce3d403e01a8a8f8f6c53b3d9e6243aa3e9b914
shan224 <shan224@gmail.com>
2017-03-01 22:29:39 -0800

    merged:

--- mobility/runt/node/reach_stream.js
+++ mobility/runt/node/reach_stream.js
@@ -9,7 +9,7 @@ var net = require('net');
 var fs = require("fs");
 var jsonfile = require('jsonfile');
 
-var file = '/home/pi/TitanRover/mobility/runt/node/gps.json'  
+var file = '/home/pi/TitanRover/mobility/runt/node/gps.json' ; 
 var reachIP = '192.168.1.112';
 var reach_shans_hotspot = '172.20.10.7';
 
