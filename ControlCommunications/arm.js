5c8140c131388a1a4f661709130aa430645c9d55
shan224 <shan224@gmail.com>
2017-03-08 14:09:08 -0800

    Changed gamepad_homebase to work with the dualshock. Moved all arm functions and dependencies to arm.js. Now we just import the module. We can test arm functions separately now instead of having to launch the server and the kitchen sink..

---
+++ ControlCommunications/arm.js
@@ -0,0 +1,231 @@
+
+/**
+ * Every movement command will be represented in 4 bytes
+ * The first byte will be the object that needs to movement
+ * in case of pwm signal object byte 3 and 4 will represent the signal between 1000 - 2000
+ * in case of stepper motor byte 2 will be direction. Byte 3 will be on or off.  Byte 4 is still
+ * up for grabs but will probably be steps
+ */
+var x_Axis_arr = new Uint16Array(2);
+x_Axis_arr[0] = 0x0800;
+var x_Axis_buff = Buffer.from(x_Axis_arr.buffer);
+
+var y_Axis_arr = new Uint16Array(2);
+y_Axis_arr[0] = 0x0900;
+var y_Axis_buff = Buffer.from(y_Axis_arr.buffer);
+
+var joint1_arr = new Uint16Array(2);
+var joint1_buff = Buffer.from(joint1_arr.buffer);
+
+var joint2_arr = new Uint16Array(2);
+joint2_arr[0] = 0x0200;
+var joint2_buff = Buffer.from(joint2_arr.buffer);
+
+var joint3_arr = new Uint16Array(2);
+joint3_arr[0] = 0x0300;
+var joint3_buff = Buffer.from(joint3_arr.buffer);
+
+var joint4_arr = new Uint16Array(2);
+var joint4_buff = Buffer.from(joint4_arr.buffer);
+
+var joint5_arr = new Uint16Array(2);
+var joint5_buff = Buffer.from(joint5_arr.buffer);
+
+var joint6_arr = new Uint16Array(2);
+var joint6_buff = Buffer.from(joint6_arr.buffer);
+
+var joint7_arr = new Uint16Array(2);
+var joint7_buff = Buffer.from(joint7_arr.buffer);
+
+module.exports = { 
+/**
+ * Joint1: Phantom Menace
+ * The rotating base for the arm
+ * Driver: Sumtor mb450a
+ */
+joint1_rotatingBase: function joint1_rotatingBase(message) {
+    let value = parseInt(message.value);
+    let direction = (value < 0) ? true : false;
+
+    if (direction) {
+        joint1_arr[0] = 0x0101;
+    } else {
+        joint1_arr[0] = 0x0100;
+    }
+
+    if (value === 0) {
+        joint1_arr[1] = 0x0000;
+    } else {
+        joint1_arr[1] = 0x0100;
+    }
+
+    port.write(joint1_buff);
+
+},
+
+/**
+ * Joint2: Attack of the Clones
+ * Will be the longer first linear Actuator
+ * Driver: Actobotics Dual Motor Controller
+ */
+joint2_linear1: function joint2_linear1(message) {
+    let value = parseInt(message.value);
+
+    value = getPwmValue(value);
+
+    joint2_arr[1] = value;
+
+    port.write(joint2_buff);
+
+},
+
+/**
+ * Joint3: Revenge of the Sith
+ * Will be the smaller second linear Actuator
+ * Driver: Actobotics Dual Motor Controller
+ */
+joint3_linear2: function joint3_linear2(message) {
+    let value = parseInt(message.value);
+
+    value = getPwmValue(value);
+
+    joint2_arr[1] = value;
+
+    port.write(joint2_buff);
+},
+
+/**
+ * Joint4: A New Hope
+ * The 180 degree wrist
+ * Driver: Sumtor mb450a
+ */
+joint4_rotateWrist: function joint4_rotateWrist(message) {
+    let value = parseInt(message.value);
+    let direction = (value < 0) ? true : false;
+
+    if (direction) {
+        joint4_arr[0] = 0x0401;
+    } else {
+        joint4_arr[0] = 0x0400;
+    }
+
+    if (value === 0) {
+        joint4_arr[1] = 0x0000;
+    } else {
+        joint4_arr[1] = 0x0100;
+    }
+
+    port.write(joint4_buff);
+},
+
+/**
+ * Joint5: Empire Strikes Back
+ * The 90 degree joint
+ * Driver: Sumtor mb450a
+ */
+joint5_90degree: function joint5_90degree(message) {
+    let value = parseInt(message.value);
+    let direction = (value < 0) ? true : false;
+
+    if (direction) {
+        joint5_arr[0] = 0x0501;
+    } else {
+        joint5_arr[0] = 0x0500;
+    }
+
+    if (value === 0) {
+        joint5_arr[1] = 0x0000;
+    } else {
+        joint5_arr[1] = 0x0100;
+    }
+
+    port.write(joint5_buff);
+},
+
+/**
+ * Joint6: Return of the Jedi
+ * 360 degree rotation of this joint no need for limit switches
+ * Driver is a Pololu
+ */
+joint6_360Unlimited: function joint6_360Unlimited(message) {
+    let value = parseInt(message.value);
+    let direction = (value < 0) ? true : false;
+
+    if (direction) {
+        joint6_arr[0] = 0x0601;
+    } else {
+        joint6_arr[0] = 0x0600;
+    }
+
+    if (value === 0) {
+        joint6_arr[1] = 0x0000;
+    } else {
+        joint6_arr[1] = 0x0100;
+    }
+
+    port.write(joint6_buff);
+},
+
+/**
+ * Joint7: The Force Awakings
+ * The gripper that is a linear actuator
+ * Driver: Pololu AMIS-30543
+ */
+joint7_gripper: function joint7_gripper(message) {
+    let value = parseInt(message.value);
+    let direction = (value < 0) ? true : false;
+
+    if (direction) {
+        joint7_arr[0] = 0x0701;
+    } else {
+        joint7_arr[0] = 0x0700;
+    }
+
+    if (value === 0) {
+        joint7_arr[1] = 0x0000;
+    } else {
+        joint7_arr[1] = 0x0100;
+    }
+
+    port.write(joint7_buff);
+},
+
+/**
+ * Tell this joint to stop moving
+ * @param {int} jointNum 1 - 7
+ */
+stopJoint: function stopJoint(jointNum) {
+    switch (jointNum) {
+        case 1:
+            joint1_arr[1] = 0x0000;
+            port.write(joint1_buff);
+            break;
+        case 2:
+            joint2_arr[1] = 0x05dc;
+            port.write(joint2_buff);
+            break;
+        case 3:
+            joint3_arr[1] = 0x05dc;
+            port.write(joint3_buff);
+            break;
+        case 4:
+            joint4_arr[1] = 0x0000;
+            port.write(joint4_buff);
+            break;
+        case 5:
+            joint5_arr[1] = 0x0000;
+            port.write(joint5_buff);
+            break;
+        case 6:
+            joint6_arr[1] = 0x0000;
+            port.write(joint6_buff);
+            break;
+        case 7:
+            joint7_arr[1] = 0x0000;
+            port.write(joint7_buff);
+            break;
+        default:
+            console.log(jointNum + " joint does not exist");
+    }
+}
+};
