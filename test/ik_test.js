d9b72dbfd4e97613bfaffa3b1dbf4c2ade2c2a42
shan224 <shan224@gmail.com>
2017-03-08 23:31:14 -0800

    Run tests using 'npm test'. It will run all the scripts in the test folder

--- test/ik_test.js
+++ test/ik_test.js
@@ -1,14 +1,16 @@
 var math = require('mathjs');
-
+var assert = require('assert');
 var l1 = 28; // Length of limb 1 inches
 var l2 = 14; // Length of limb 2 inches
 
-j1_bound = math.pi/4;
-j2_bound = -1* math.pi/4;
+J1_BOUND = math.pi/4;
+J2_BOUND = -1* math.pi/4;
 var X ; 
 var Y ; 
 var THETA1D;
+
 var THETA2D;
+
 
 // Given the XY, output the Thetas
 function inverse_kinematics(X,Y,l1,l2){
@@ -29,13 +31,19 @@ function forward_kinematics(THETA1D,THETA2D,l1,l2){
   Y = l1*math.sin(THETA1D) + l2*math.sin(THETA1D+THETA2D);
 }
 
-// Testing all values within bounds
-for(i = 0; i <=j1_bound; i+=0.01){
-  for(j = 0; j >=j2_bound; j-=0.01){
-    console.log("Given theta1  : " + THETA1D+ " Given theta2: " + THETA2D);
-    forward_kinematics(i,j,l1,l2);
-    inverse_kinematics(X,Y,l1,l2);
-    console.log("Deduced Theta1: " + THETA1D+  " Deduced Theta2: "+ THETA2D);
-    console.log("\n");
+// Chai Test
+for(i = 0; i <=J1_BOUND; i+=0.01){
+  for(j = 0; j >=J2_BOUND; j-=0.01){
+    describe("IK test for given Theta1D: " + i.toFixed(4) + " Theta2D: "+ j.toFixed(4),function(){
+      it('should equal the deduced theta',function(done){
+        forward_kinematics(i,j,l1,l2);
+        inverse_kinematics(X,Y,l1,l2);
+        assert.deepEqual(THETA1D.toFixed(4),i.toFixed(4), "THETA1D SHOULD BE EQUAL");
+        assert.deepEqual(THETA2D.toFixed(4),j.toFixed(4),"THETA2D SHOULD BE EQUAL");
+        done();
+      });
+    });
   }
 }
+
+
