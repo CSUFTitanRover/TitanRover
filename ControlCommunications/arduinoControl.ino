d8174422b6028b035496bbdc950b1bb650077621
Joseph Porter <joporter5606@live.com>
2017-03-09 15:04:37 -0800

    Added joint stepping for the arm.  Will allow us to step a certain joint 1 - 200 steps.  Limited to 200 steps because this is a 360 rotation.

--- ControlCommunications/arduinoControl.ino
+++ ControlCommunications/arduinoControl.ino
@@ -56,6 +56,7 @@ bool joint7_on = false;
 // Will hold the bytes from the pi
 uint8_t val[4];
 int i;
+int delayVal = 1;
 uint16_t pwmVal;
 
 void setup()
@@ -84,13 +85,13 @@ void setup()
   pinMode(joint1_pulse_pin, OUTPUT);
   digitalWrite(joint1_enab_pin, LOW);
 
-    // Set up Joint4
+  // Set up Joint4
   pinMode(joint4_dir_pin, OUTPUT);
   pinMode(joint4_enab_pin, OUTPUT);
   pinMode(joint4_pulse_pin, OUTPUT);
   digitalWrite(joint4_enab_pin, LOW);
 
-    // Set up Joint5
+  // Set up Joint5
   pinMode(joint5_dir_pin, OUTPUT);
   pinMode(joint5_enab_pin, OUTPUT);
   pinMode(joint5_pulse_pin, OUTPUT);
@@ -117,12 +118,12 @@ void loop()
 {
 
   // Check if pi has sent command data to Arduino
-  if(Serial.available() >= 4)
+  if (Serial.available() >= 4)
   {
 
     // Grap the 4 bytes from the raspberry pi
     i = 0;
-    while(i < 4)
+    while (i < 4)
     {
       val[i++] = Serial.read();
     }
@@ -132,44 +133,54 @@ void loop()
     pwmVal = (uint16_t)val[2];
     pwmVal = pwmVal | ((uint16_t)val[3] << 8);
 
-    if(val[0] == 0x01)  // Joint1
+    if (val[0] == 0x01) // Joint1
     {
       setDirection(joint1_dir_pin, (bool)val[1]);
       joint1_on = val[2];
+      if (val[3] != 0x00)
+        stepJoint(joint1_pulse_pin, val[3]);
     }
-    else if(val[0] == 0x02) // Joint2
+    else if (val[0] == 0x02) // Joint2
     {
       joint2.writeMicroseconds(pwmVal);
     }
-    else if(val[0] == 0x03) // Joint3
+    else if (val[0] == 0x03) // Joint3
     {
       joint3.writeMicroseconds(pwmVal);
     }
-    else if(val[0] == 0x04) // Joint4
+    else if (val[0] == 0x04) // Joint4
     {
       setDirection(joint4_dir_pin, (bool)val[1]);
       joint4_on = val[2];
+      if (val[3] != 0x00)
+        stepJoint(joint4_pulse_pin, val[3]);
     }
-    else if(val[0] == 0x05) // Joint5
+    else if (val[0] == 0x05) // Joint5
     {
       setDirection(joint5_dir_pin, (bool)val[1]);
       joint5_on = val[2];
+      if (val[3] != 0x00)
+        stepJoint(joint5_pulse_pin, val[3]);
     }
-    else if(val[0] == 0x06) // Joint6
+    else if (val[0] == 0x06) // Joint6
     {
       setDirection(joint6_dir_pin, (bool)val[1]);
       joint6_on = val[2];
+      if (val[3] != 0x00)
+        stepJoint(joint6_pulse_pin, val[3]);
     }
-    else if(val[0] == 0x07) // Joint7
+    else if (val[0] == 0x07) // Joint7
     {
       setDirection(joint7_dir_pin, (bool)val[1]);
       joint7_on = val[2];
+      if (val[3] != 0x00)
+        stepJoint(joint7_pulse_pin, val[3]);
     }
-    else if(val[0] == 0x08) // X Axis mobility
+    else if (val[0] == 0x08) // X Axis mobility
     {
       x_mobility.writeMicroseconds(pwmVal);
     }
-    else if(val[0] == 0x09) // Y Axis mobility
+    else if (val[0] == 0x09) // Y Axis mobility
     {
       y_mobility.writeMicroseconds(pwmVal);
     }
@@ -178,44 +189,55 @@ void loop()
 
 
   // Drive the stepper motors if they are activated
-  if(joint1_on)
+  if (joint1_on)
   {
     digitalWrite(joint1_pulse_pin, HIGH);
     digitalWrite(joint1_pulse_pin, LOW);
   }
 
-  if(joint4_on)
+  if (joint4_on)
   {
     digitalWrite(joint4_pulse_pin, HIGH);
     digitalWrite(joint4_pulse_pin, LOW);
   }
 
-  if(joint5_on)
+  if (joint5_on)
   {
     digitalWrite(joint5_pulse_pin, HIGH);
     digitalWrite(joint5_pulse_pin, LOW);
   }
 
-  if(joint6_on)
+  if (joint6_on)
   {
     digitalWrite(joint6_pulse_pin, HIGH);
     digitalWrite(joint6_pulse_pin, LOW);
   }
 
-  if(joint7_on)
+  if (joint7_on)
   {
     digitalWrite(joint7_pulse_pin, HIGH);
     digitalWrite(joint7_pulse_pin, LOW);
   }
 
-  delay(1);
+  delay(delayVal);
 
+}
+
+// Will step the joint between 1 and 200 steps
+void stepJoint(uint8_t pinValue, uint8_t steps)
+{
+  for (int i = 0; i < steps; ++i)
+  {
+    digitalWrite(pinValue, HIGH);
+    digitalWrite(pinValue, LOW);
+    delay(delayVal);
+  }
 }
 
 // Will switch the pin based on what byte is sent from the pi
 void setDirection(uint8_t pinValue, bool val)
 {
-  if(val)
+  if (val)
   {
     digitalWrite(pinValue, LOW);
   }
