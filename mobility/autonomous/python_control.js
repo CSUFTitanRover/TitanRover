ccebad0cbaf9d11c75166684c3f525d50aaeafa6
shan224 <shan224@gmail.com>
2017-03-31 21:46:44 -0700

    refactored

--- mobility/autonomous/python_control.js
+++ mobility/autonomous/python_control.js
@@ -1,65 +1,48 @@
 var PythonShell = require('python-shell');
+var options = {
+	  mode: 'text',
+	  pythonPath: '/usr/bin/python',
+	  pythonOptions: ['-u'],
+	  scriptPath: './python3',
+  	  args: []
+	};
 
 module.exports = {
     move_forward: function(throttle){
-        var options = {
-	  mode: 'text',
-	  pythonPath: '/usr/bin/python',
-	  pythonOptions: ['-u'],
-	  scriptPath: './python3',
-  	  args: ['f',throttle]
-	};
-
-	PythonShell.run('pyShell_handler', options, function (err, results) {
-	  if (err) throw err;
-	  // results is an array consisting of messages collected during execution
-	  console.log('results: %j', results);
-	});
+      options.args[0] = 'f';
+			options.args[1] = throttle;
+			PythonShell.run('pyShell_handler', options, function (err, results) {
+				if (err) throw err;
+				// results is an array consisting of messages collected during execution
+				console.log('results: %j', results);
+			});
     },
     stop: function(){
-      var options = {
-	  mode: 'text',
-	  pythonPath: '/usr/bin/python',
-	  pythonOptions: ['-u'],
-	  scriptPath: './python3',
-  	  args: ['s']
-	};
-
-	PythonShell.run('pyShell_handler', options, function (err, results) {
-	  if (err) throw err;
-	  // results is an array consisting of messages collected during execution
-	  console.log('results: %j', results);
-	});
-    },
+   		options.args[0] = 's';
+			PythonShell.run('pyShell_handler', options, function (err, results) {
+				if (err) throw err;
+				// results is an array consisting of messages collected during execution
+				console.log('results: %j', results);
+			});
+		
+		},
     turn_right: function(throttle){
-        var options = {
-	  mode: 'text',
-	  pythonPath: '/usr/bin/python',
-	  pythonOptions: ['-u'],
-	  scriptPath: './python3',
-  	  args: ['r',throttle]
-	};
-
-	PythonShell.run('pyShell_handler.py', options, function (err, results) {
-	  if (err) throw err;
-	  // results is an array consisting of messages collected during execution
-	  console.log('results: %j', results);
-	});
+      options.args[0] = 'r';
+			options.args[1] = throttle;
+			PythonShell.run('pyShell_handler', options, function (err, results) {
+				if (err) throw err;
+				// results is an array consisting of messages collected during execution
+				console.log('results: %j', results);
+			});
     },
     turn_left: function(throttle){
-        var options = {
-	  mode: 'text',
-	  pythonPath: '/usr/bin/python',
-	  pythonOptions: ['-u'],
-	  scriptPath: './python3',
-  	  args: ['l',throttle]
-	};
-
-	PythonShell.run('pyShell_handler', options, function (err, results) {
-	  if (err) throw err;
-	  // results is an array consisting of messages collected during execution
-	  console.log('results: %j', results);
-	});
+     options.args[0] = 'l';
+		 options.args[1] = throttle;
+		 PythonShell.run('pyShell_handler', options, function (err, results) {
+				if (err) throw err;
+				// results is an array consisting of messages collected during execution
+				console.log('results: %j', results);
+			});
     },
    
 };
