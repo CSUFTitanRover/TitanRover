b6b394e3c7d66718f16a880fc242be43f66ea3c5
HawkinsonB <brandon.hawkinson@icloud.com>
2017-04-13 18:00:44 -0700

    Finished logic changes to test on the runt

--- mobility/autonomous/ForwardP.js
+++ mobility/autonomous/ForwardP.js
@@ -22,12 +22,16 @@ var rover = require('./runt_pyControl.js');
 var pwm_min = 1100; // Calculated to be 1000 us
 var pwm_max = 4095; // Calculated to be 2000 us
 var drive_constant = 2600;
+var acceptable_Degree_Error = 1;
 var currentLeftThrottle;
 var currentRightThrottle;
 var previousRightThrottle;
 var previousLeftThrottle;
 var current_heading;
 var heading_delta;
+
+var turning_left;
+var turning_right;
 
 var driveCounter;
 
@@ -67,19 +71,11 @@ var forwardPMovement = function() {
     winston.info('----ForwardPmovement----')
     rover.drive_forward();
     drive_timer = setInterval(function() {
-        temp_heading_delta = current_heading - target_heading;
-        if (current_heading > target_heading) {
-            if(Math.abs(temp_heading_delta > 180)) {
-                winston.info('Right Hand Heading');
-                heading_delta = 360 - current_heading + target_heading;
-            }
-        }else{
-            heading_delta = 360 - current_heading + target_heading;
-        }
+        calc_heading_delta();
         winston.info("Current Heading: " + current_heading);
         winston.info("Target Heading: " + target_heading);
         winston.info("Heading Delta: " + heading_delta)
-        if (Math.abs(heading_delta) <= 1) {
+        if (Math.abs(heading_delta) <= acceptable_Degree_Error) {
             rover.set_speed(drive_constant, drive_constant);
             winston.info('Moving forward at drive constant');
             driveCounter++;
@@ -87,15 +83,17 @@ var forwardPMovement = function() {
             //Calculate the throttle percentage change based on what the proportion is.
             throttlePercentageChange = heading_delta/180
             calcThrottleMultiplier();
-            if(heading_delta > 0){
+            if(turning_right && !turning_left){
                     winston.info('Slowing turning right');
                     currentLeftThrottle = drive_constant + (drive_constant * throttleMultiplier);
                     currentRightThrottle = drive_constant - (drive_constant * throttleMultiplier);
-            }else{
+            }else if(turning_left && !turning_right){
                     winston.info('Slowing turning left');
                     currentLeftThrottle = drive_constant - (drive_constant * throttleMultiplier);
                     currentRightThrottle = drive_constant + (drive_constant * throttleMultiplier);
-                    }
+            } else {
+                winston.info('ERROR - Cannot slowly turn left or right');
+            }
         }
         //Checks to see if the currentThrottle values are valid for mechanical input as it is possible that the values can be significantly more or
         //less than pwm_min and pwm_max. Then sets the rover speed to the calculated value
@@ -140,83 +138,48 @@ var forwardPMovement = function() {
     });
 };
 
-//Reworking logic for this function
-var forwardPMovementTest = function() {
-    winston.info("Calculating Current Deviation")
+//grabbed shan's calc_heading_delta() that we worked on together for the turning/heading logic
+function calc_heading_delta(){
+    
+    temp_delta = current_heading - target_heading;
 
-    //executes the forward movement for the rover
-    rover.drive_forward();
-
-    //Begin checking for any errors during our traversal and adjusting that over time. 
-    drive_timer = setInterval(function(){
-        //winston.info( 'Turning ... Current heading: ' + current_heading + ' Target heading: ' + target_heading.toFixed(2));
-        var heading_delta = current_heading - target_heading; 
-            if(current_heading > target_heading){
-                if(Math.abs(heading_delta) > 180){
-                    heading_delta = 360 - current_heading + target_heading;
-                    winston.info("Current Heading: " + current_heading);
-                    winston.info("Target Heading: " + target_heading);
-                    winston.info("Heading Delta: " + heading_delta)
-                    winston.info('slowing turning right');
-                    throttlePercentageChange = heading_delta/90
-                    calcThrottleMultiplier();
-                    right_scale_error_factor = 2995 * throttleMultiplier + pwm_min;
-                    left_scale_error_factor = pwm_max;
-                }else{
-                    heading_delta = current_heading - target_heading;
-                    winston.info("Current Heading: " + current_heading);
-                    winston.info("Target Heading: " + target_heading);
-                    winston.info("Heading Delta: " + heading_delta)
-                    winston.info('slow turning left');
-                    throttlePercentageChange = heading_delta/90;
-                    calcThrottleMultiplier();
-                    left_scale_error_factor = 2995 * throttleMultiplier + pwm_min;
-                    right_scale_error_factor = pwm_max;
-                    }
-            }else{
-                if(Math.abs(heading_delta) > 180){
-                    heading_delta = 360 - target_heading + current_heading;
-                    winston.info("Current Heading: " + current_heading);
-                    winston.info("Target Heading: " + target_heading);
-                    winston.info("Heading Delta: " + heading_delta)
-                    winston.info('slowly turning left');
-                    throttlePercentageChange = heading_delta/90
-                    calcThrottleMultiplier();
-                    left_scale_error_factor = 2995 * throttleMultiplier + pwm_min;
-                    right_scale_error_factor = pwm_max;
-
-                }else{
-                    heading_delta = target_heading - current_heading;
-                    winston.info("Current Heading: " + current_heading);
-                    winston.info("Target Heading: " + target_heading);
-                    winston.info("Heading Delta: " + heading_delta)
-                    winston.info('slowly turning right');
-                    throttlePercentageChange = heading_delta/90
-                    calcThrottleMultiplier();
-                    right_scale_error_factor = 2995 * throttleMultiplier + pwm_min;
-                    left_scale_error_factor = pwm_max;
-                }
+// Is turning left or right the shorter turn?
+    if(current_heading > target_heading){
+        if(Math.abs(temp_delta) > 180){
+            // If we were turning left previously or have never turned right before
+            if(turning_left || turning_right === null){
+                winston.info('turning right: '+ current_heading);
+                turning_right = true;
+                turning_left = false;
             }
-
-        // If we are within x degrees of the desired heading stop, else check if we overshot
-        winston.info('Delta test: ' + heading_delta);
-        if(Math.abs(heading_delta) <= 1){
-            rover.drive_forward();
-            rover.set_speed()
-            clearInterval(turn_timer);
-            //winston.info('on_target: + current heading');
-            setTimeout(function(){winston.info(current_heading)},1000);
+            heading_delta = 360 - current_heading + target_heading;
+        }else{
+              // If we were turning right previously or have never turned left before
+             if(turning_right || turning_left === null){
+                winston.info('turning left: '+ current_heading);
+                turning_left = true;
+                turning_right = false;
+            }
+            heading_delta = current_heading - target_heading;
+            }
+    }else{
+        if(Math.abs(temp_delta) > 180){ 
+             if(turning_right || turning_left === null){
+                winston.info('turning left: '+ current_heading);
+                turning_left = true;
+                turning_right = false;
+            }
+            heading_delta = 360 - target_heading + current_heading;
+        }else{
+            if(turning_left || turning_right === null){
+                winston.info('turning right: '+ current_heading);
+                turning_right = true;
+                turning_left = false;
+            }
+            heading_delta = target_heading - current_heading;
         }
-        else if(previous_heading_delta !== null && previous_heading_delta < heading_delta){
-            clearInterval(turn_timer);
-            forwardPMovement();
-        }
-        previous_heading_delta = heading_delta;
-        winston.info(left_scale_error_factor);
-        winston.info(right_scale_error_factor);
-        rover.set_speed(left_scale_error_factor, right_scale_error_factor);
-    },15);    
-};
+    }
+}; 
 
 var calcThrottleMultiplier = function() {
     winston.info("Throttle Percentage Change: " + throttlePercentageChange);
