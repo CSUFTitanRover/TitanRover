34388c8b9c74034482edbb1dd3721a1cd7ff1f28
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2017-04-18 00:25:52 -0700

    Create RuntMotor.py
    Process the commands for the Motor hat from the Node.js program calls.  Several dependencies from adafruit required.  Values to run the motors are passed in as arguments.

---
+++ mobility/autonomous/motor_hat/RuntMotor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+########## Code dependencies #######################################################
+##  git clone https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library.git
+##  cd Adafruit-Motor-HAT-Python-Library
+##  sudo apt-get install python-dev
+##  sudo python setup.py install
+####################################################################################
+
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time,sys
+import atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+    #print('Motors stopped')
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+if sys.argv[1] == 0 and sys.argv[2] == 0:
+    atexit.register(turnOffMotors)
+else:
+####### runt motors run!#########
+   #####Right Side######
+
+    mh.getMotor(1).setSpeed(argv[1]);
+    mh.getMotor(2).setSpeed(argv[1]);
+
+   #####Left Side######
+    mh.getMotor(3).setSpeed(argv[2]);
+    mh.getMotor(4).setSpeed(argv[2]);
+
+    #Text output for testing code
+    #print('right side power ' + sys.argv[1] + ' left side power ' + sys.argv[2])
+    #sys.stdout.flush()
