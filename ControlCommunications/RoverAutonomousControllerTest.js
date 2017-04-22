70fbdd868d00992f83bed9ea45e6c8769fa322a9
HawkinsonB <brandon.hawkinson@icloud.com>
2017-04-21 17:56:26 -0700

    Created a autonomous controller testbed until we get std communication down between the two

---
+++ ControlCommunications/RoverAutonomousControllerTest.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var serialPort = require('serialport');
+
+var port = new serialPort('/dev/ttyACM0', {
+    baudRate: 9600
+});
+
+var x_Axis_arr = new Uint16Array(2);
+x_Axis_arr[0] = 0x0008;
+var x_Axis_buff = Buffer.from(x_Axis_arr.buffer);
+
+var y_Axis_arr = new Uint16Array(2);
+y_Axis_arr[0] = 0x0009;
+var y_Axis_buff = Buffer.from(y_Axis_arr.buffer);
+
+var time = new Date();
+
+function setYAxis(speed) {
+    if (speed < -127 || speed > 127) {
+        throw new RangeError('speed must be between -127 and 127');
+    }
+
+    // Since we are using unsigened ints for serial make it between 0 and 254
+    y_Axis_arr[1] = speed + 127;
+    port.write(y_Axis_buff);
+}
+
+function setXAxis(speed) {
+    if (speed < -127 || speed > 127) {
+        throw new RangeError('speed must be between -127 and 127');
+    }
+
+    // Since we are using unsigned ints for serial make it between 0 and 254
+    x_Axis_arr[1] = speed + 127;
+    port.write(x_Axis_buff);
+}
+
+function stopRover() {
+    //receiveMobility(zeroMessage[0]);
+    //receiveMobility(zeroMessage[1]);
+    x_Axis_arr[1] = 127;
+    y_Axis_arr[1] = 127;
+    port.write(x_Axis_buff);
+    port.write(y_Axis_buff);
+    // Stopping all joints
+    for (i = 1; i <= 7; i++) {
+        port.write(arm.stopJoint(i));
+    }
+}
+
+var main = setInterval(function(){
+    if(current_heading != null){
+        //clearInterval(main);
+        for (var i = 0; i < 255; i++) {
+           setYAxis(i);
+           setXAxis(i);
+        }
+        stopRover();
+        //setTimeout(function(){;},1000);
+    }
+    
+},500);
+
+process.on('SIGTERM', function() {
+    console.log("STOPPING ROVER");
+    stopRover();
+    port.close();
+    process.exit();
+});
+
+process.on('SIGINT', function() {
+    console.log("\n####### JUSTIN LIKES MENS!! #######\n");
+    console.log("\t\t╭∩╮（︶︿︶）╭∩╮");
+    stopRover();
+    port.close();
+    // some other closing procedures go here
+    process.exit();
+});
