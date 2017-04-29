aef4dadb56562807501a8a76787d273f2c37acb1
HawkinsonB <brandon.hawkinson@icloud.com>
2017-04-29 12:08:00 -0700

    Added serial port to TankDrive for testing

--- mobility/autonomous/ForwardP_Tankdrive.js
+++ mobility/autonomous/ForwardP_Tankdrive.js
@@ -13,6 +13,86 @@ var process = spawn('python',["/home/pi/TitanRover/mobility/autonomous/python3/I
 var rover = require('./runt_pyControl.js');
 */
 
+var port = new serialPort('/dev/ttyACM0', {
+    baudRate: 9600,
+    parser: serialPort.parsers.readline('\n')
+});
+
+
+var x_Axis_arr = new Uint16Array(3);
+x_Axis_arr[0] = 0xB;
+x_Axis_arr[2] = 0xbbaa;
+var x_Axis_buff = Buffer.from(x_Axis_arr.buffer);
+
+var y_Axis_arr = new Uint16Array(3);
+y_Axis_arr[0] = 0xC;
+y_Axis_arr[2] = 0xbbaa;
+var y_Axis_buff = Buffer.from(y_Axis_arr.buffer);
+
+var time = new Date();
+var timer;
+function setLeftSide(leftSpeed) {
+    if (leftSpeed < -127 || leftSpeed > 127) {
+        throw new RangeError('speed must be between -127 and 127');
+    }
+    console.log('Y: ' + leftSpeed );
+    // Since we are using unsigened ints for serial make it between 0 and 254
+    leftSpeed = leftSpeed + 127;
+    parseInt(leftSpeed);
+    y_Axis_arr[1] = leftSpeed;
+    //x_Axis_arr[1] = parseInt(speed + 127);
+
+    //console.log(y_Axis_buff);
+    console.log(y_Axis_arr);
+    console.log(y_Axis_buff);
+    port.write(y_Axis_buff);
+    //port.write(x_Axis_buff)
+}
+
+function setRightSide(rightSpeed) {
+    if (rightSpeed < -127 || rightSpeed > 127) {
+        throw new RangeError('speed must be between -127 and 127');
+    }
+    console.log('x: ' + rightSpeed);
+    // Since we are using unsigned ints for serial make it between 0 and 254
+    rightSpeed = rightSpeed + 127;
+    parseInt(rightSpeed);
+    x_Axis_arr[1] = rightSpeed;
+    console.log(x_Axis_arr);
+    console.log(x_Axis_buff);
+    port.write(x_Axis_buff);
+}
+
+function driveForward(leftSideThrottle, rightSideThrottle) {
+    setRightSide(rightSideThrottle);
+    setLeftSide(leftSideThrottle);
+}
+
+function stopRover() {
+    //receiveMobility(zeroMessage[0]);
+    //receiveMobility(zeroMessage[1]);
+    x_Axis_arr[1] = 127;
+    y_Axis_arr[1] = 127;
+    port.write(x_Axis_buff);
+    port.write(y_Axis_buff);
+    // Stopping all joints
+
+}
+// Any serial data from the arduino will be sent back home
+// and printed to the console
+port.on('data', function(data) {
+    console.log('ArduinoMessage: ' + data);
+    var jsonBuilder = {};
+    jsonBuilder.ArduinoMessage = data;
+    jsonBuilder.type = 'debug';
+    //ssendHome(jsonBuilder);
+});
+
+port.on('open',function(){
+    console.log('open');
+    //setTimeout(main,1000);
+});
+
 //DRIVE-CONSTANT: 
 //40 - UNTESTED, UNSURE OF SPEED, BE AWARE WHEN TESTING
 var drive_constant = 40;
@@ -87,6 +167,7 @@ var forwardPMovement = function() {
 
         if (leftThrottle < throttle_max && leftThrottle > throttle_min &&  rightThrottle < throttle_max && rightThrottle > throttle_min){
             //rover.set_speed(Math.trunc(leftThrottle), Math.trunc(rightThrottle));
+            driveForward(leftThrottle, rightThrottle);
             //PUT SET SPEED IN HERE.
             previousLeftThrottle = leftThrottle;
             previousRightThrottle = rightThrottle;
@@ -101,7 +182,7 @@ var forwardPMovement = function() {
                 leftThrottle = throttle_min;
             } else {
                 console.log('ERROR - leftThrottle values undefined');
-                //rover.stop();
+                stopRover();
                 clearInterval(drive_timer);
             }
 
@@ -112,16 +193,17 @@ var forwardPMovement = function() {
                 rightThrottle = throttle_min;
             } else {
                 console.log('ERROR - rightThrottle values undefined');
-                //rover.stop();
+                stopRover();
                 clearInterval(drive_timer);
             }
             //PUT SET SPEED HERE AS WELL
+            driveForward(leftThrottle, rightThrottle);
             console.log("Setting rover speed - Left: " + leftThrottle + ", right:" + rightThrottle);
         }
 
         if (driveCounter > 361) {
             clearInterval(drive_timer);
-            //rover.stop();
+            stopRover();
             console.log('On Heading...Stopping...');
         } else {
             console.log('Thottle Adjusted');
@@ -180,3 +262,20 @@ var main = setInterval(function(){
     }
     
 },500);
+
+process.on('SIGTERM', function() {
+    console.log("STOPPING ROVER");
+    clearInterval(timer);
+    stopRover();   
+});
+
+process.on('SIGINT', function() {
+    console.log("\n####### JUSTIN LIKES MENS!! #######\n");
+    console.log("\t\t╭∩╮（︶︿︶）╭∩╮");
+    clearInterval(timer);
+    stopRover();
+    //port.close();
+    process.exit(port.close());
+    // some other closing procedures go here
+
+});
