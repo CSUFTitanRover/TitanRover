c87ece6e770b82b6b93a6aea5123516e2a74002d
shan224 <shan224@gmail.com>
2017-05-28 22:09:00 -0700

    reading waypoints from file now

--- mobility/autonomous/atlas_events_refactor.js
+++ mobility/autonomous/atlas_events_refactor.js
@@ -4,18 +4,26 @@ var geolib = require('geolib');
 var spawn = require("child_process").spawn;
 var net = require('net');
 var magneticDeclination = 0; //12.3 in Fullerton, 15 in Hanksville
-var python_proc = spawn('python',["/home/pi/TitanRover/GPS/IMU/Python_Version/IMU_Acc_Mag_Gyro.py", magneticDeclination]);
-
+//var python_proc = spawn('python',["/home/pi/TitanRover/GPS/IMU/Python_Version/IMU_Acc_Mag_Gyro.py", magneticDeclination]);
+var port = require('serialport');
 var now = require('performance-now');
 var moment = require('moment'); //gets system time
 var Winston = require('winston');
+var fs = require('fs');
+ var file = '/home/pi/TitanRover/mobility/autonomous/gps.json';
 
 //----VARIABLES----
+//----IMU Client Socket-----
+// var imu_net = require('net'),
+//     imu_port = 9015,
+//     imu_host = 'localhost'
+//     imu_socket = imu_net.createConnection(imu_port,imu_host);
+//----End IMU Socket--------
 
 //FOR OFF ROVER TESTING:
-//Inject a current_heading, if not, leave undefined ex: var current_heading; You may also adjust target heading depending on when we have waypoints
-var current_heading; //leave untouched
-var target_heading = 65; //change to desired target heading, will be replaced post calculation of GPS data
+//Inject a currentHeading, if not, leave undefined ex: var currentHeading; You may also adjust target heading depending on when we have waypoints
+var currentHeading; //leave untouched
+var targetHeading = 65; //change to desired target heading, will be replaced post calculation of GPS data
 
 // Timers
 var turn_timer; 
@@ -23,66 +31,84 @@ var drive_timer;
 
 var distance;          // distance to next waypoint
 var onTargetRange = 5; // in meters, distance we want to start to modify the drive throttle to slow the rover down as we approach a waypoint
-var onTargetError = 0.25; //distance we need to stop at a waypoint
+var onTargetError = 1; //distance we need to stop at a waypoint
 onTargetRange = geolib.convertUnit('cm',onTargetRange); // immediately convert from meters to cm for comparisons.
 onTargetError = geolib.convertUnit('cm',onTargetError); // immediately convert from meters to cm for comparisons.
 
 //DRIVE-CONSTANTS: 
-var turning_drive_constant = 60; //Turning Motor Constant
-var forward_drive_constant = 50; //ForwardP Motor Constant
-var forward_drive_modifier = 0; //to modify when driving straight forward
+var turning_drive_constant = 35; //Turning Motor Constant
+var forward_drive_constant = 30; //ForwardP Motor Constant
+var forward_drive_modifier = 1; //to modify when driving straight forward
 
 //DEGREES OF ERROR
-var turning_drive_error = 7;         // within 20 degrees stop turn
+var turning_drive_error = 3;         // within 20 degrees stop turn
 var forward_drive_to_turn_error = 10; //logic to exit forwardP and turn. 
 var forward_drive_error = 3;          //within 4 degrees drive straight
 
 //THROTTLE LOGIC
-var turning_throttle_min = 50;      // Minimum throttle value to move the rover
-var turning_throttle_max = 70;      // Maximum throttle value acceptable
-var forward_throttle_min = 40; //Minimum throttle value acceptable
-var forward_throttle_max = 60; //Maximum throttle value acceptable
+var turning_throttle_min = 25;      // Minimum throttle value to move the rover
+var turning_throttle_max = 60;      // Maximum throttle value acceptable
+var forward_throttle_min = 25; //Minimum throttle value acceptable
+var forward_throttle_max = 45; //Maximum throttle value acceptable
 var proportional_error;        // heading ratio, used as modifer for throttle
 var distanceModifier;       // distance ratio, used as modifer for throttle
 var distanceThreshold = 20; // We need to be within x cm of target to move on 
 var current_wayPoint = -1;  // -1 because index is incremented immediately; 
 var currentLocation = null;        // JSON - latitude and longitude {latitude: 124141, longitude:3515}
 var previousLocation = null;    // JSON - latitude and longitude {latitude: 124141, longitude:3515}
+var previousHeading = 0;
 // BOOLEAN LOGIC FOR FUNCTIONS
 var turn_right = null;
 var onTarget = false;
+var badHeadingData = false;
+var badLocationData = false;
+var invalidHeadingCounter = 0;
+var invalidLocationCounter = 0;
+
+var previousLocation = {
+    latitude: 0,
+    longitude: 0
+};
 
 //-----WAYPOINTS-----
-var wayPoints = [
-    {latitude: 33.907797253, longitude: -117.888230827}, // 1
-    {latitude: 33.907813544, longitude: -117.888253258}, // 2 
-    {latitude: 33.90784173, longitude: -117.888263651}, // 3
-    {latitude: 33.907855246, longitude: -117.888282362}, // 4 
-    {latitude: 33.907840636, longitude: -117.888309203}, // 5
-    {latitude: 33.907855246, longitude: -117.888282362},  // 6
-    {latitude: 33.90784173, longitude: -117.888263651},
-    {latitude: 33.907813544, longitude: -117.888253258}, 
-    {latitude: 33.907797253, longitude: -117.888230827}
-     ]; 
+//for parking lot at CSUF
+/*
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1
+     {latitude: 33.88210346, longitude: -117.88163178}, // 2
+     {latitude: 33.88210607, longitude: -117.88168939}, // 3
+     {latitude: 33.88214708, longitude: -117.88173965}, // 4
+     {latitude: 33.88210346, longitude: -117.88163178}, // 2
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1 
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1
+     {latitude: 33.88214708, longitude: -117.88173965}, //  4 
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1
+     {latitude: 33.88210346, longitude: -117.88163178}, // 2
+     {latitude: 33.88210607, longitude: -117.88168939}, // 3
+     {latitude: 33.88214708, longitude: -117.88173965}, // 4
+     {latitude: 33.88210346, longitude: -117.88163178}, // 2
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1 
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1
+     {latitude: 33.88214708, longitude: -117.88173965}, //  4 
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1
+     {latitude: 33.88210346, longitude: -117.88163178}, // 2
+     {latitude: 33.88210607, longitude: -117.88168939}, // 3
+     {latitude: 33.88214708, longitude: -117.88173965}, // 4
+     {latitude: 33.88210346, longitude: -117.88163178}, // 2
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1 
+     {latitude: 33.88215365, longitude: -117.88161841}, // 1
+     {latitude: 33.88214708, longitude: -117.88173965} //  4 
+*/
 
-//  var wayPoints = [
-//     {latitude: 33.9077992, longitude: -117.88823026}, // 1
-//     {latitude: 33.90799779, longitude: -117.88817478}, // 2 
-//     {latitude: 33.90795127, longitude: -117.88819359}, // 3
-//     {latitude: 33.90795765, longitude: -117.88818212}, // 4 
-//     {latitude: 33.90796292, longitude: -117.88821231}, // 5
-//     {latitude: 33.90799283, longitude: -117.88822578}  // 6
-//     ]; 
+var wayPoints = JSON.parse(fs.readFileSync(file, 'utf8'));
 //-----END WAYPOINTS-----
 //-----WINSTON ENTRY POINT-----
 const winston = new (Winston.Logger)({
     transports: [
       new (Winston.transports.Console)({
           'colorize': true
-          
      }),
       new (Winston.transports.File)({
-          filename: './autonomous_logs/autonomous_' + moment.format() + '.log',
+          filename: './autonomous_logs/autonomous_' + moment().format() + '.log',
           options:{flags: 'w'}, // Overwrite logfile. Remove if you want to append 
           timestamp: function () {
           return now();},
@@ -100,7 +126,7 @@ client.connect(9001, reachIP, function() {
 client.on('data', function(data,err) { 
     winston.info("***reachClient***");
     if(err){
-        winston.info("Error!: " + JSON.stringify(err));
+        winston.info("Error: " + JSON.stringify(err));
     }
     // Parse the data into an array
     data = data.toString().split(" ").filter(theSpaces);
@@ -108,27 +134,84 @@ client.on('data', function(data,err) {
             latitude: data[2],
             longitude: data[3]
         };
-    
+    if (badLocationData && (previousLocation.lattitude != currentLocation.lattitude || previousLocation.longitude != currentLocation.longitude)) { 
+        badLocationData = false;
+    }
+    winston.info("Updated GPS Location: ", currentLocation);
 });
-//----END REACH CODE----
-//----IMU ENTRY----
-var invalidHeadingCounter = 0;
-python_proc.stdout.on('data', function (data){   
-    data = parseFloat(data); 
-    winston.info("***python_proc.stdout***");
-    winston.info("IMU Data:")
+
+//  UNIX SOCKET 
+var imu_client = new net.Socket();
+
+imu_client.connect('/home/pi/TitanRover/GPS/IMU/Python_Version/imu_sock', function(){
+    winston.info("Connected to IMU via UNIX socket ");
+});
+
+imu_client.on('data',function(data,err){
+    if(err){
+        winston.info('Error: ', err);
+    }
+    data = parseFloat(data);
+    winston.info("*** UNIX sockets ***");
+    winston.info("IMU Data: " + data);
     if (isNaN(data)) {
-        winston.info("Current Heading is NaN");
-        stopRover();
+        winston.info("ERROR: Current Heading is NaN");
         clearInterval(turn_timer);
         clearInterval(drive_timer);
+        stopRover();
     } else if ( 0 <= data && data <= 360){
-        current_heading = data;
+        currentHeading = data;
+        if (badHeadingData && (currentHeading != previousHeading)) {
+            badHeadingData = false;
+        }
     } else {
         winston.info("ERROR: IMU Heading Out of Range: " + data);
     }
 });
-//----END IMU----
+imu_client.on('end',function(){
+   console.log('ERROR: imu disconnected'); 
+});
+//----END REACH CODE----
+//----IMU ENTRY----
+// python_proc.stdout.on('data', function (data){   
+//     data = parseFloat(data); 
+//     winston.info("***python_proc.stdout***");
+//     winston.info("IMU Data: " + data);
+//     if (isNaN(data)) {
+//         winston.info("Current Heading is NaN");
+//         clearInterval(turn_timer);
+//         clearInterval(drive_timer);
+//         stopRover();
+//     } else if ( 0 <= data && data <= 360){
+//         currentHeading = data;
+//     } else {
+//         winston.info("ERROR: IMU Heading Out of Range: " + data);
+//     }
+// });
+
+// imu_socket.on('data', function(data){
+//     data = parseFloat(data); 
+//     //winston.info("***python_proc.stdout***");
+//     //winston.info("IMU Data: " + data);
+//     if (isNaN(data)) {
+//         winston.info("Current Heading is NaN");
+//         clearInterval(turn_timer);
+//         clearInterval(drive_timer);
+//         stopRover();
+//     } else if ( 0 <= data && data <= 360){
+//         currentHeading = data;
+//     } else {
+//         winston.info("ERROR: IMU Heading Out of Range: " + data);
+//     }
+// });
+// imu_socket.on('SIGINT',function(){
+//     imu_socket.exit();
+// });
+
+// imu_socket.on('end',function(){
+//    console.log('imu disconnected'); 
+// })
+// //----END IMU----
 
 // *** TEMP CHECK TO STOP ROVER 
 // var locationCounter = 0 ; 
@@ -170,9 +253,8 @@ atlas.on('get_waypoint',function(){
        winston.info("reached end of waypoints");
     }
     else{
-        onTarget = false;
-        target_heading = geolib.getBearing(currentLocation,wayPoints[current_wayPoint]);
-        winston.info("get_waypoint adjusted Target Heading:" + target_heading);
+        targetHeading = geolib.getBearing(currentLocation,wayPoints[current_wayPoint]);
+        winston.info("get_waypoint adjusted Target Heading:" + targetHeading);
         atlas.emit('turn');
     }
 });
@@ -193,11 +275,16 @@ atlas.on('turn',function(){
         winston.info('***turn_timer***');
         calc_heading_delta();
         // if we're within our error we drive
-        if (Math.abs(heading_delta) <= turning_drive_error) {
+        if (badHeadingData || badLocationData) {
+            winston.info("ERROR: Bad Data Stopping Rover")
+            winston.info("badHeadingData: ", badHeadingData);
+            winston.info("badLocationData: ", badLocationData);
+            stopRover();
+        } else if (Math.abs(heading_delta) <= turning_drive_error) {
             clearInterval(turn_timer);
             winston.info('***FOUND HEADING EMITTING DRIVE***');
-            winston.info('Current Heading: ' + current_heading + " Target Heading: " + target_heading);
-            winston.info("Final heading before drive event: ", current_heading);
+            winston.info('Current Heading: ' + currentHeading + " Target Heading: " + targetHeading);
+            winston.info("Final heading before drive event: ", currentHeading);
             winston.info("heading delta:" + heading_delta);
             stopRover();
             atlas.emit('drive');
@@ -240,11 +327,16 @@ atlas.on('drive',function(){
         winston.info("***drive_timer***")
         pathfinder();           //gets distance, distanceModifier modifier and target heading
         calc_heading_delta();   //calculates best turn towards target heading
-        
-        if (distance < onTargetError) {
+        if (badHeadingData || badLocationData) {
+            winston.info("ERROR: Bad Data Stopping Rover")
+            winston.info("badHeadingData: ", badHeadingData);
+            winston.info("badLocationData: ", badLocationData);
+            stopRover();
+        }
+        else if (distance < onTargetError) {
             clearInterval(drive_timer);
             stopRover(); 
-            winston.log("***AT WAYPOINT***")
+            winston.log("***AT WAYPOINT***");
             atlas.emit('get_waypoint');
         }
         else if (Math.abs(heading_delta) > forward_drive_to_turn_error) { //removed || distance < distance_threshold 
@@ -270,9 +362,9 @@ atlas.on('drive',function(){
             winston.info('turn_right:' + turn_right);
             //throttle_offset = Math.round(forward_drive_constant * proportional_error * 1.5 * Math.log(heading_delta));
 
-            y = Math.log(forward_throttle_max / forward_drive_constant) * proportional_error;
-            throttle_offset = Math.round(forward_drive_constant * (Math.pow(Math.E, y) * proportional_error));
-            throttle_offset = Math.round(throttle_offset * heading_delta);
+            y = Math.log(forward_throttle_max) * proportional_error;
+            throttle_offset = 2 * Math.round(forward_drive_constant * (Math.pow(Math.E, y)));
+            //throttle_offset = Math.round(throttle_offset * heading_delta);
             winston.info("forwardP Throttle Offset: " + throttle_offset);
 
             if(turn_right){
@@ -300,25 +392,36 @@ atlas.on('drive',function(){
 function calc_heading_delta(){
     //Check for non-updated heading values from the IMU
     winston.info('***calc_heading_delta***');
-    if(current_heading == previousHeading) {
+    if(currentHeading == previousHeading) {
         invalidHeadingCounter++;
-        winston.info("IMU Heading same as previous");
+        winston.info("Invalid Heading, counter: ", invalidHeadingCounter);
     } else {
-        previousHeading = current_heading;
+        previousHeading = currentHeading;
         invalidHeadingCounter = 0;
         winston.info("IMU Heading validified");
     }
-    if (invalidHeadingCounter >= 20) {
-        clearInterval(drive_timer);
-        clearInterval(turn_timer);
-        stopRover();
-        winston.info("ERROR: IMU Heading Invalid");
+    if (previousLocation.lattitude === currentLocation.lattitude && previousLocation.longitude === currentLocation.longitude) {
+        invalidLocationCounter++;
+        winston.info("Invalid Location, counter: ", invalidLocationCounter);
+    } else {
+        previousLocation = currentLocation;
+        invalidLocationCounter = 0;
+        winston.info("GPS Location validified");
     }
-    let abs_delta = Math.abs(current_heading - target_heading);
-    winston.info("Current Heading: " + current_heading);
-    winston.info("Target Heading: " + target_heading);
+    if (invalidHeadingCounter >= 50 || invalidLocationCounter >= 10) {
+        if (invalidHeadingCounter >= 50) {
+            winston.info("ERROR: IMU Heading Invalid", currentHeading);
+            badHeadingData = true;
+        } else {
+            winston.info("ERROR: GPS Location Invalid: ", currentLocation);
+            badLocationData = true;
+        }
+    }
+    let abs_delta = Math.abs(currentHeading - targetHeading);
+    winston.info("Current Heading: " + currentHeading);
+    winston.info("Target Heading: " + targetHeading);
     // xnor operation also note (!turn_right = turn_left)
-    turn_right = current_heading > target_heading === abs_delta > 180; 
+    turn_right = currentHeading > targetHeading === abs_delta > 180; 
     heading_delta = abs_delta <= 180 ? abs_delta : 360 - abs_delta;
 }
 
@@ -327,17 +430,28 @@ function calc_heading_delta(){
 function pathfinder() {
     //This geolib function needs to be (myGPSlocation,current_waypointGPSlocation) 
     winston.info("***pathfinder***")
+    winston.info("Current Location: ", currentLocation);
+    winston.info("Target Location: ",wayPoints[current_wayPoint]);
+    var tempDistance = distance;
+    try {
     distance = geolib.getDistance(currentLocation,wayPoints[current_wayPoint],1,5);
     distance = geolib.convertUnit('cm',distance);
-    winston.info("Current Location: ", currentLocation);
-    winston.info("Target Location: "+ wayPoints[current_wayPoint]);
     winston.info("Distance: ", distance);
+    } catch(e) {
+        winston.info("GEOLIB ERROR: ", e);
+        distance = tempDistance;
+    }
     //This geolib function needs to be (myGPSlocation,current_waypointGPSlocation)
-    target_heading = geolib.getBearing(currentLocation,wayPoints[current_wayPoint]);
-    winston.info("pathfinder updated Target Heading: " + target_heading);
-    output_nav_data();
+    var tempTargetHeading = targetHeading;
+    try {
+        targetHeading = geolib.getBearing(currentLocation,wayPoints[current_wayPoint]);
+    } catch (e){
+        winston.info("GEOLIB ERROR: ", e);
+        targetHeading = tempTargetHeading;
+    }
+    winston.info("pathfinder updated Target Heading: " + targetHeading);
     if (distance <= onTargetRange) {
-        winston.info("***ON TARGET RANGE***")
+        winston.info("***ON TARGET RANGE***");
         distanceModifier = distance / onTargetRange;
         winston.info("Distance Modifier: " + distanceModifier);
     } else {
@@ -364,11 +478,15 @@ Number.prototype.clamp = function(min, max) {
 //INCLUDED FOR CONTROL OF THE ROVER WITH UPDATED SERIAL COMMUNICATION
 //-------ROVERCONTROL------
 var serialPort = require('serialport');
+
 var port = new serialPort('/dev/ttyACM0', {
     baudRate: 9600,
     parser: serialPort.parsers.readline('\n')
 });
 
+port.on('error',function(err){
+    console.log('Serialport error: ', err);
+});
 var left_side_arr = new Uint16Array(3);
 left_side_arr[0] = 0xB;
 left_side_arr[2] = 0xbbaa;
@@ -457,11 +575,16 @@ process.on('SIGINT', function() {
         process.exit();
     }, 1000);
 });
+
+process.on('uncaughtException', function (err) {
+  winston.info('Caught exception: ' + err);
+});
+
 //----END SCRIPT KILL----
 //----END ROVER CONTROL----
 
 var main = setInterval(()=> {
-    if(currentLocation !== null && current_heading !== null){
+    if(currentLocation !== null && currentHeading !== null){
         winston.info("***EVENTS_REFACTORED BOOTED***")
         clearInterval(main);
         setTimeout(() => atlas.emit('get_waypoint'), 3000);
